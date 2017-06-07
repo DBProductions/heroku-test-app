59997799313b1b9007bd730d4a3d68b9eab7b5da
DBProductions <dennis@dbproductions.de>
2017-06-07 19:14:23 +0200

    show environment

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const ENVIRONMENT = process.env.NODE_ENV;
 
 app.get('/', (req, res) => {
-    res.send('Hello Heroku Pipeline!')
+    res.send('Hello Heroku ' + ENVIRONMENT + '!')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, () => {
+    console.log('Example app listening on port ' + PORT + '!')
 })
