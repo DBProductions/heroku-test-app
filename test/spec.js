1824ccd30d001319c5ef53570f8db14a0cd7d539
DBProductions <dennis@dbproductions.de>
2017-06-11 18:47:55 +0200

    adjust test

--- test/spec.js
+++ test/spec.js
@@ -1,29 +1,29 @@
 var hippie = require('hippie');
 var server = require('../lib/server');
 
-describe('Server', function () {
-  describe('/ endpoint', function () {
-    it('returns a user based on the id', function (done) {
-      hippie(server)
-        .json()
-        .get('/')
-        .expectStatus(200)
-        .end(function(err, res, body) {
-          if (err) throw err;
-          done();
+describe('Server', () => {
+    describe('/ endpoint', () => {
+        it('returns the basic data', (done) => {
+            hippie(server)
+            .json()
+            .get('/')
+            .expectStatus(200)
+            .end((err, res, body) => {
+                if (err) throw err;
+                done();
+            });
         });
     });
-  });
-  describe('/wrong endpoint', function () {
-    it('returns a user based on the id', function (done) {
-      hippie(server)
-        .json()
-        .get('/wrong')
-        .expectStatus(404)
-        .end(function(err, res, body) {
-          if (err) throw err;
-          done();
+    describe('/wrong endpoint', () => {
+        it('returns the expected error', (done) => {
+            hippie(server)
+            .json()
+            .get('/wrong')
+            .expectStatus(404)
+            .end((err, res, body) => {
+                if (err) throw err;
+                done();
+            });
         });
     });
-  });
 });
