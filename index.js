b2318db251fdaae60740dbe191bb221c06f8f238
DBProductions <dennis@dbproductions.de>
2017-06-11 20:01:19 +0200

    use the rest router

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var server = require('./lib/server');
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log('Listening on port: %s', PORT);
+    console.log('%s listening at %s', server.name, server.url);
 });
