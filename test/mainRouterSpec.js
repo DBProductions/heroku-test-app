197a3e22884bc70500003c78d9023f9404744d88
DBProductions <dennis@dbproductions.de>
2017-06-20 20:02:59 +0200

    seperate tests

--- test/mainRouterSpec.js
+++ test/mainRouterSpec.js
@@ -1,25 +1,13 @@
 var hippie = require('hippie');
 var server = require('../lib/server');
 
-describe('Server', () => {
+describe('mainRouter', () => {
     describe('/ endpoint', () => {
         it('returns the basic data', (done) => {
             hippie(server)
             .json()
             .get('/')
             .expectStatus(200)
-            .end((err, res, body) => {
-                if (err) throw err;
-                done();
-            });
-        });
-    });
-    describe('/wrong endpoint', () => {
-        it('returns the expected error', (done) => {
-            hippie(server)
-            .json()
-            .get('/wrong')
-            .expectStatus(404)
             .end((err, res, body) => {
                 if (err) throw err;
                 done();
