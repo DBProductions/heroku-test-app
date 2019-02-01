b18132dab6adaf5bacde5541d2e92088e65e0537
DBProductions <dennis@dbproductions.de>
2019-02-01 23:06:27 +0100

    switch to fastify

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-'use strict';
 const server = require('./lib/server');
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log('%s listening at %s', server.name, server.url);
+server.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Listening at %s', PORT);
 });
