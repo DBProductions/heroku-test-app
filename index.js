093ea857c28b20ce6468322eb641dd34f9014622
DBProductions <dennis@dbproductions.de>
2019-02-01 23:22:43 +0100

    set host

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./lib/server');
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, (err) => {
+server.listen(PORT, '0.0.0.0', (err) => {
     if (err) {
         console.log(err);
     }
